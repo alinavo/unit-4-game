25bbb98c4631105e5e26b556995748e6567bf628
alinavo <alinahuynhvo@gmail.com>
2018-12-01 21:46:07 -0500

    css fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,64 @@
 
-//name variables (random number, count, user wins, and losses)
+//variables
 var number = "";
 var wins = 0;
 var losses = 0;
 var counter = 0;
-var images = ["../assets/images/cup.jpg", "../assets/images/diadem.jpg", "../assets/images/locket.jpg", "../assets/images/ring.jpg"];
+var images = ["..assets/images/locket.jpg", "..assets/images/cup.jpg", "..assets/images/diadem.jpg", "..assets/images/ring.jpg"];
 
 // Functions
 
-	function randomNumer () {
-		number = Math.floor(Math.random() * 102) + 19;
+function randomNumber() {
+ number = Math.floor(Math.random() * 102) + 19;
 	}
 
-	function resetHorcruxes () {
-		for (var i = 0; i < images.length; i++) {
-			var horcrux = $("<img>"); 
-			horcrux.addClass("horcrux"); //not sure if I need this
-			horcrux.attr("src", images[i]);
-			horcrux.attr("value", (Math.floor(Math.random() * 12) + 1));
-			horcrux.attr("height", "100");
-			$(".horcrux-images").append(horcrux);
-		}
+function resetHorcruxes() {
+	for (var i = 0; i < images.length; i++) {	
+		var horcrux = $("<img>");
+		horcrux.addClass("horcrux");
+		horcrux.attr("src", images[i]);
+		horcrux.attr("value", (Math.floor(Math.random() * 12) + 1));
+		horcrux.attr("height", "100");
+		$(".horcrux-images").append(horcrux);
 	}
+}
 
-	function resetHTML () {
-		$(".target-number").html(number);
-		$(".win-lose-counter").html("<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
-		$(".score-number").html(counter);
-		$(".horcrux-images").empty();
-	}
+function resetHTML() {
+	$(".target-number").html(number);
+	$(".win-lose-counter").html("<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
+	$(".score-number").html(counter);
+	$(".horcrux-images").empty();
+}
 
-	function totalReset () {
-		randomNumer ();
-		counter = 0;
-		resetHTML ();
-		resetHorcruxes ();
-	}
+function totalReset() {
+	randomNumber();
+	counter = 0;
+	resetHTML();
+	resetHorcruxes();
+}
 
 // Running Code
 
-// Sets page up to run random number functin
-  randomNumer();
-//runs random number function
-  resetHTML (); 
-//RESET RESET RESET !!!!!!
- resetHorcruxes ();
+// Inital Page Set Up
+resetHTML();
+resetHorcruxes();
+randomNumber();
 
-// click functions for horcruxs to add numbers
-	function horcruxClick () {
-    //attr returns first value of selected html element
-    //use parseInt to change to integer  
-		counter += parseInt($(this).attr("value"));
-		$(".score-number").html(counter);
-		if (counter == number) {
-			wins++;
-			totalReset();
-		}
-		else if (counter > number) {
-			losses++;
-			totalReset();
-		};
+
+// Click Functions
+function horcruxClick() {
+	//attr returns first value of selected html element
+	counter += parseInt($(this).attr("value"));
+	$(".score-number").html(counter);
+	if (counte) {
+		wins++;
+		totalReset();
+	}
+	else if (count) {
+		losses++;
+		totalReset();
 	};
+};
 
-	//Throughout life cycle of the document, accounting for every single time document is dynamically changed execute horcruxClick function
-	$(document).on("click", ".horcrux", horcruxClick);
+//Throughout life cycle of the document, accounting for every single time document is dynamically changed execute crystalClick function
+$(document).on("click", ".horcrux", horcruxClick);
