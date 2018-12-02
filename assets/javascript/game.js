90bcb423f843cb478ce69df4bb6d088ddaab58e9
alinavo <alinahuynhvo@gmail.com>
2018-12-01 22:32:50 -0500

    push changes in html and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,45 +4,53 @@ var number = "";
 var wins = 0;
 var losses = 0;
 var counter = 0;
-var images = ["..assets/images/locket.jpg", "..assets/images/cup.jpg", "..assets/images/diadem.jpg", "..assets/images/ring.jpg"];
+var images = ["assets/images/locket.jpg", "assets/images/cup.jpg", "assets/images/diadem.jpg", "assets/images/ring.jpg"];
 
-// Functions
-
+// Functions 
 function randomNumber() {
- number = Math.floor(Math.random() * 102) + 19;
-	}
+	number = Math.floor(Math.random() * 102) + 19;
+ //assigns target number to var number
+console.log(number)};
 
+
+
+//resets photo
 function resetHorcruxes() {
-	for (var i = 0; i < images.length; i++) {	
+	for (var i = 0; i < images.length; i++) {
+		//image
 		var horcrux = $("<img>");
-		horcrux.addClass("horcrux");
+		//adds class horcruxes
+		horcrux.addClass("horcruxes");
+		// image from array
 		horcrux.attr("src", images[i]);
+		// adds random value
 		horcrux.attr("value", (Math.floor(Math.random() * 12) + 1));
 		horcrux.attr("height", "100");
 		$(".horcrux-images").append(horcrux);
-	}
+	};
+	
 }
-
+//reset HTML page
 function resetHTML() {
-	$(".target-number").html(number);
+	$(".target-number").html(number); //prints number in html
 	$(".win-lose-counter").html("<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
-	$(".score-number").html(counter);
+	$(".score-number").html(counter); //prints score in html
 	$(".horcrux-images").empty();
-}
-
+};
+resetHTML();
 function totalReset() {
 	randomNumber();
 	counter = 0;
 	resetHTML();
 	resetHorcruxes();
-}
+};
 
 // Running Code
 
-// Inital Page Set Up
+randomNumber();
 resetHTML();
 resetHorcruxes();
-randomNumber();
+
 
 
 // Click Functions
@@ -50,15 +58,15 @@ function horcruxClick() {
 	//attr returns first value of selected html element
 	counter += parseInt($(this).attr("value"));
 	$(".score-number").html(counter);
-	if (counte) {
+	if (counter) {
 		wins++;
 		totalReset();
 	}
-	else if (count) {
+	else if (counter) {
 		losses++;
 		totalReset();
 	};
 };
 
-//Throughout life cycle of the document, accounting for every single time document is dynamically changed execute crystalClick function
-$(document).on("click", ".horcrux", horcruxClick);
+//click for every horcrux
+$(document).on("click", ".horcruxes", horcruxClick);
